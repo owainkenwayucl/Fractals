933478336c5a99aa94f8047e20647d43dd5f52f9
Owain Kenway <o.kenway@ucl.ac.uk>
2018-09-26 18:53:30 +0100

    Examples as per Wikipedia
    https://en.wikipedia.org/wiki/Julia_set#Examples_of_Julia_sets

--- julia.py
+++ julia.py
@@ -1,4 +1,10 @@
 import fractals
 
-j = fractals.generate_julia(0.279,2)
-fractals.show_image(fractals.generate_fractal(600,600, j, max_iter=50, xmin=-2.0, xmax=2.0, ymin=-2.0, ymax=2.0))
+# Generate julia sets as per https://en.wikipedia.org/wiki/Julia_set#Examples_of_Julia_sets
+
+n = 2
+c = [0.279, 0.400, 0.484, 0.544, 0.590, 0.626]
+for i in c: 
+    j = fractals.generate_julia(i,n)
+    fractals.show_image(fractals.generate_fractal(600,600, j, max_iter=50, xmin=-2.0, xmax=2.0, ymin=-2.0, ymax=2.0))
+    n += 1
