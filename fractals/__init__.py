3fd6ab325c0281d0efd3a7463b659d708861cc69
Owain Kenway <o.kenway@ucl.ac.uk>
2018-09-26 17:55:44 +0100

    Aute determine colour range when drawing image.

--- fractals/__init__.py
+++ fractals/__init__.py
@@ -25,11 +25,13 @@ def generate_fractal(width, height, func, xmin=-2, xmax=1, ymin=-1, ymax=1, max_
             image[px,height - py - 1] = func(c,max_iter)
     return image
 
-def show_image(image, max_iter=MAX_ITERATIONS):
+def show_image(image):
     import tkinter
 
     width = image.shape[0]
     height = image.shape[1]
+
+    maxcolour = numpy.amax(image)
 
     window = tkinter.Tk()
     window.title("Fractal Image")
@@ -37,7 +39,7 @@ def show_image(image, max_iter=MAX_ITERATIONS):
     canvas.pack(expand=tkinter.YES, fill=tkinter.BOTH) 
     for py in range(height):
         for px in range(width):
-            shade = hex(int((float(image[px,py])/float(max_iter)) * 256)-1)[2:]
+            shade = hex(int((float(image[px,py])/float(maxcolour)) * 256)-1)[2:]
             if len(shade) == 1:
                 shade='0' + shade
             colour = '#' + shade + shade + shade
